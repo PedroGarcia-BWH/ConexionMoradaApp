8be5f6e7d3ebc0fe70022d090f8070af340efcee
saikora11 <pedro.garciaromera@alum.uca.es>
2023-01-14 18:22:08 +0100

    Update Login

--- app/src/main/java/es/uca/tfg/conexionmorada/LoginActivity.kt
+++ app/src/main/java/es/uca/tfg/conexionmorada/LoginActivity.kt
@@ -2,11 +2,41 @@ package es.uca.tfg.conexionmorada
 
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var txtEmail: TextView
+    private lateinit var txtPassword: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        txtEmail = findViewById(R.id.email)
+        txtPassword = findViewById(R.id.password)
+        val logLogin = findViewById<Button>(R.id.login)
+        val progressBar = findViewById<ProgressBar>(R.id.progress)
+
+        logLogin.setOnClickListener(View.OnClickListener {
+            val email: String
+            val password: String
+            email = txtEmail.getText().toString()
+            password = txtPassword.getText().toString()
+            if (email != "" && password != "") {
+                progressBar.setVisibility(View.VISIBLE)
+                //InicioSesion(email,password)
+                progressBar.setVisibility(View.GONE)
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Todos los campos son requeridos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
     }
 }
---
+++ app/src/main/java/es/uca/tfg/conexionmorada/security/Login.kt
@@ -0,0 +1,4 @@
+package es.uca.tfg.conexionmorada.security
+
+class Login {
+}
