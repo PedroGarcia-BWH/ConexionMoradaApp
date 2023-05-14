40c0fee5ccc570b3b506f71f6b04aa562b19d72d
saikora11 <pedro.garciaromera@alum.uca.es>
2023-05-14 15:14:05 +0200

    Map

--- app/src/main/java/es/uca/tfg/conexionmorada/ui/SistemaCompaneroFragment.kt
+++ app/src/main/java/es/uca/tfg/conexionmorada/ui/SistemaCompaneroFragment.kt
@@ -1,16 +1,33 @@
 package es.uca.tfg.conexionmorada.ui
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import es.uca.tfg.conexionmorada.R
-
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 class SistemaCompaneroFragment : Fragment() {
+    private lateinit var mMap: GoogleMap
+    private val REQUEST_LOCATION_PERMISSION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Obtener permiso para la ubicación del usuario
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+        }
     }
 
     override fun onCreateView(
@@ -21,7 +38,51 @@ class SistemaCompaneroFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sistema_companero, container, false)
     }
 
-    override  fun onActivityCreated(savedInstanceState: Bundle?) {
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        // Inicializar el mapa
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync { googleMap ->
+            mMap = googleMap
+            mMap.uiSettings.isZoomControlsEnabled = false
+            checkLocationPermissionAndSetCenter()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permiso concedido, centrar el mapa en la ubicación
+                checkLocationPermissionAndSetCenter()
+            } else {
+                // Permiso denegado, mostrar mensaje al usuario
+                Toast.makeText(requireContext(), "Permiso de ubicación denegado", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun checkLocationPermissionAndSetCenter() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // El permiso de ubicación no está concedido, no se puede centrar el mapa en la ubicación
+            return
+        }
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                val latLng = LatLng(location.latitude, location.longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14f))
+            }
+        }
     }
 }
